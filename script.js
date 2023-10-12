9b62233286f1d98b5ab6d495f7d0c50ab9a1d394
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-10-12 21:20:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 //your code here
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-touristSpots.sort(a,b=>(a-b));
+// An array of band names
+let bandNames = ['The Rolling Stones', 'Aerosmith', 'Led Zeppelin', 'Pink Floyd'];
+
+// Function to remove articles from band names for sorting
+function removeArticles(name) {
+  return name.replace(/^(the|a|an) /i, '').trim();
+}
+
+// Sort the band names lexicographically after removing articles
+bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+// Get the <ul> element by its ID
+let bandList = document.getElementById('band');
+
+// Loop through the sorted band names and create <li> elements
+for (let name of bandNames) {
+  let listItem = document.createElement('li');
+  listItem.textContent = name;
+  bandList.appendChild(listItem);
+}
